364d0fbf63b843937f580eab00c1417af7270bb0
Simon Löfgren <simonrlofgren@hotmail.com>
2022-01-08 14:09:57 +0100

    multiprocessing on linux

--- genetic_algorithm_tsp.py
+++ genetic_algorithm_tsp.py
@@ -3,7 +3,7 @@ from util import get_points_for_algorithms, get_distance_matrix_for_algorithm
 import numpy as np
 import matplotlib.pyplot as plt
 from sko.GA import GA_TSP
-
+from sko.tools import set_run_mode
 """
 Use city_long_lat.csv to create 120 points of long and lat.
 Makes a distance matrix in shape (120, 120) out of the calculated distances between two cities.
@@ -25,16 +25,19 @@ def cal_total_distance(routine):
     return sum([distance_matrix[routine[i % num_points], routine[(i + 1) % num_points]] for i in range(num_points)])
 
 
-# %% do GA
-
-ga_tsp = GA_TSP(func=cal_total_distance, n_dim=num_points, size_pop=50, max_iter=500, prob_mut=0.001)
 
 
-start_time = datetime.datetime.now()
-best_points, best_distance = ga_tsp.run()
-print('on {max_iter} iterations, {prob_mut} mutations, costs {time_costs}s'
-      .format(max_iter=ga_tsp.max_iter, prob_mut=ga_tsp.prob_mut,
-              time_costs=(datetime.datetime.now() - start_time).total_seconds()))
+for mode in ('common', 'multiprocessing'):
+    set_run_mode(cal_total_distance, mode)
+    for max_iter in (100, 500, 1000):
+        for prob_mut in (0.001, 0.01, 0.05):
+            ga_tsp = GA_TSP(func=cal_total_distance, n_dim=num_points, size_pop=50, max_iter=max_iter, prob_mut=prob_mut)
+
+            start_time = datetime.datetime.now()
+            best_points, best_distance = ga_tsp.run()
+            print('on {max_iter} iterations, {prob_mut} mutations, costs {time_costs}s, mode {mode}'
+                  .format(max_iter=ga_tsp.max_iter, prob_mut=ga_tsp.prob_mut,
+                          time_costs=(datetime.datetime.now() - start_time).total_seconds(), mode=mode))
 
 
 # %% plot
