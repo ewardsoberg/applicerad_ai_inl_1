46c4d3f2123de18e66cd606e75df7027a98b4eac
Simon Löfgren <simonrlofgren@hotmail.com>
2022-01-08 17:01:46 +0100

    multiprocessing

--- genetic_algorithm_tsp.py
+++ genetic_algorithm_tsp.py
@@ -3,7 +3,8 @@ from util import get_points_for_algorithms, get_distance_matrix_for_algorithm
 import numpy as np
 import matplotlib.pyplot as plt
 from sko.GA import GA_TSP
-from sko.tools import set_run_mode
+import multiprocessing as mp
+import time
 """
 Use city_long_lat.csv to create 120 points of long and lat.
 Makes a distance matrix in shape (120, 120) out of the calculated distances between two cities.
@@ -18,32 +19,57 @@ num_points = len(points_coordinate)
 
 
 def cal_total_distance(routine):
-    '''The objective function. input routine, return total distance.
+    '''
+    The objective function. input routine, return total distance.
     cal_total_distance(np.arange(num_points))
     '''
     num_points, = routine.shape
     return sum([distance_matrix[routine[i % num_points], routine[(i + 1) % num_points]] for i in range(num_points)])
 
+def ga_tsp_single():
+    for max_iter in (100, 500, 1000):
+        for prob_mut in (0.001, 0.01, 0.05):
+            ga_tsp = GA_TSP(func=cal_total_distance, n_dim=num_points, size_pop=50, max_iter=500, prob_mut=0.001)
 
+            start_time = datetime.datetime.now()
+            best_points, best_distance = ga_tsp.run()
 
+            print('on {max_iter} iterations, {prob_mut} mutations, costs {time_costs}s'
+                  .format(max_iter=ga_tsp.max_iter, prob_mut=ga_tsp.prob_mut,
+                          time_costs=(datetime.datetime.now() - start_time).total_seconds()))
 
-for mode in ('common', 'multiprocessing'):
-    set_run_mode(cal_total_distance, mode)
+def ga_tsp_multi():
     for max_iter in (100, 500, 1000):
         for prob_mut in (0.001, 0.01, 0.05):
             ga_tsp = GA_TSP(func=cal_total_distance, n_dim=num_points, size_pop=50, max_iter=max_iter, prob_mut=prob_mut)
 
             start_time = datetime.datetime.now()
             best_points, best_distance = ga_tsp.run()
-            print('on {max_iter} iterations, {prob_mut} mutations, costs {time_costs}s, mode {mode}'
+
+            print('multiprocessing on {max_iter} iterations, {prob_mut} mutations, costs {time_costs}s'
                   .format(max_iter=ga_tsp.max_iter, prob_mut=ga_tsp.prob_mut,
-                          time_costs=(datetime.datetime.now() - start_time).total_seconds(), mode=mode))
+                          time_costs=(datetime.datetime.now() - start_time).total_seconds()))
+
+def main():
+
+    n_cores = mp.cpu_count()
+    print(f'Number of cores: {n_cores}')
+
+    start_time = datetime.datetime.now()
+
+    processes = [mp.Process(target=ga_tsp_multi) for i in range(n_cores)]
+
+    # Starta varje process
+    [p.start() for p in processes]
+    # Sammanställ processer
+    [p.join() for p in processes]
+    # Plocka ut resultat
+
+    print(f' multi time {datetime.datetime.now()-start_time}')
 
 
-# %% plot
-fig, ax = plt.subplots(1, 2)
-best_points_ = np.concatenate([best_points, [best_points[0]]])
-best_points_coordinate = points_coordinate[best_points_, :]
-ax[0].plot(best_points_coordinate[:, 0], best_points_coordinate[:, 1], 'o-r')
-ax[1].plot(ga_tsp.generation_best_Y)
-plt.show()
+
+if __name__ == "__main__":
+    
+    mp.freeze_support()
+    main()
